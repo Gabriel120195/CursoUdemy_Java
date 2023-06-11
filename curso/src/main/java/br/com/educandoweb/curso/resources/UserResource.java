aa7eda93d0d1a58841c97d963377b8fdc0a058c9
Gabriel120195 <gabriel-1295@hotmail.com>
2023-06-11 00:50:42 -0300

    Até aula 302

---
+++ curso/src/main/java/br/com/educandoweb/curso/resources/UserResource.java
@@ -0,0 +1,35 @@
+package br.com.educandoweb.curso.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.educandoweb.curso.entities.User;
+import br.com.educandoweb.curso.services.UserService;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+        
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = userService.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+    
+}
